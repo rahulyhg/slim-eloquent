5a0a94b5d6a6db780fc6d187940e5bbfc29bc7b7
Gabriel Baker <me@g403.co>
2015-09-12 00:55:09 +0100

    Initial refactor to make eloquent module external

---
+++ src/dependencies.php
@@ -0,0 +1,8 @@
+<?php
+$container['model.structure'] = function($container) {
+    return realpath(__DIR__.'/../templates').'/ModelTemplate.txt');
+};
+
+$container['SlimApi\Model\ModelInterface'] = function($container) {
+    return new \SlimApi\Model\EloquentModelService($container->get('model.structure'), $container->get('namespace.root'));
+};
